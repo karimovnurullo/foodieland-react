85ae14e7dd5495feefe0086530972c6d4763b8f1
Nurullo <karimovdeveloper@gmail.com>
2023-06-09 19:20:03 +0500

    New commit

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "./components/header/header";
 import Home from "./components/home/home";
+import Categories from "./components/categories/categories";
 
 export default class App extends React.Component {
   render() {
@@ -8,6 +9,7 @@ export default class App extends React.Component {
       <>
         <Header />
         <Home />
+        <Categories />
       </>
     );
   }
---
+++ src/components/categories/categories.jsx
@@ -0,0 +1,52 @@
+import React,{ Component } from 'react';
+
+import img1 from "./photos/image1.png";
+import img2 from "./photos/image2.png";
+import img3 from "./photos/image3.png";
+import img4 from "./photos/image4.png";
+import img5 from "./photos/image5.png";
+import img6 from "./photos/image6.png";
+
+import "./categories.scss";
+class Categories extends Component {
+   render() { 
+      return (
+         <div class="categories">
+        <div class="row">
+            <h2>Categories</h2>
+            <div>
+                <p>View All Categories</p>
+            </div>
+        </div>
+        <div class="card_box">
+            <div class="card">
+                <img src={img1} alt="" />
+                <p>Breakfast</p>
+            </div>
+            <div class="card">
+                <img src={img2} alt=""/>
+                <p>Breakfast</p>
+            </div>
+            <div class="card">
+                <img src={img3} alt=""/>
+                <p>Breakfast</p>
+            </div>
+            <div class="card">
+                <img src={img4} alt=""/>
+                <p>Breakfast</p>
+            </div>
+            <div class="card">
+                <img src={img5} alt=""/>
+                <p>Breakfast</p>
+            </div>
+            <div class="card">
+                <img src={img6} alt=""/>
+                <p>Breakfast</p>
+            </div>
+        </div>
+    </div>
+      );
+   }
+}
+ 
+export default Categories;
